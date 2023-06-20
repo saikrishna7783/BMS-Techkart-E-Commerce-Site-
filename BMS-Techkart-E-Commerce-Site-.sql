7dc543ba8e1c1af10e6481e3d820e25708250876
saikrishna7783 <saikrishna7783@outlook.com>
2023-06-20 21:01:42 +0530

    updated the mysql code

--- BMS-Techkart-E-Commerce-Site-.sql
+++ BMS-Techkart-E-Commerce-Site-.sql
@@ -7,15 +7,31 @@ create table users(
 	email_id varchar(264),
     pwd varchar(264)
 );
+select * from users;
+truncate table users;
 
 create table audience(
 audiencename varchar(264),
 email_id varchar(264),
 comments varchar(264)
 );
+select * from audience;
+truncate table audience;
 
 create table orders(
 email_id varchar(264),
 product_title varchar(264),
 product_price varchar(264)
 );
+select * from orders;
+truncate table orders;
+
+create table checkout(
+fullname varchar(264),
+email varchar(264),
+tel varchar(264),
+address varchar(264),
+upi_id varchar(264)
+);
+select * from checkout;
+truncate table checkout;
